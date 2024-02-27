508445f9e1aa574497e32cf2b59d7771ec3d21c2
petitbouquet <161125945+petitbouquet@users.noreply.github.com>
2024-02-28 08:12:44 +0900

    add tehai struct

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,53 @@
 <script setup>
+    import { ref } from 'vue'
+
+    let mjTehaiStrings = ref('')
+
+    let mjActions = ref({
+        isReached: false,
+        isDoubleReached: false,
+        isTsumo: false,
+        isIppatu: false,
+        isHaitei: false,
+        isRinsyan: false,
+        isChankan: false
+    })
+
+    let mjBlocks = [
+        { kind: 'manzu', type: 'syuntsu', isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: false, values: [1, 2, 3] },
+        { kind: 'souzu', type: 'syuntsu', isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: false, values: [1, 2, 3] },
+        { kind: 'pinzu', type: 'syuntsu', isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: false, values: [1, 2, 3] },
+        { kind: 'ton', type: 'koutsu', isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: true, values: [1, 1, 1] },
+        { kind: 'haku', type: 'jyanto', isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: true, values: [1, 1] },
+    ]
 </script>
 
 <template>
   <div>
     <p>hello</p>
+    <input v-model="mjTehaiStrings">
+    <p>mjTehaiStrings: {{mjTehaiStrings}} </p>
+    <div>
+        {{ JSON.stringify(mjActions) }}
+    </div>
+    <p>mjActions Button</p>
+    <div>
+        <button @click="mjActions.isReached = !mjActions.isReached">isReached</button>
+        <button @click="mjActions.isDoubleReached = !mjActions.isDoubleReached">isDoubleReached</button>
+        <button @click="mjActions.isTsumo = !mjActions.isTsumo">isTsumo</button>
+        <button @click="mjActions.isIppatu = !mjActions.isIppatu">isIppatu</button>
+        <button @click="mjActions.isHaitei = !mjActions.isHaitei">isHaitei</button>
+        <button @click="mjActions.isRinsyan = !mjActions.isRinsyan">isRinsyan</button>
+        <button @click="mjActions.isChankan = !mjActions.isChankan">isChankan</button>
+    </div>
+    <br>
+    <div>
+        {{ JSON.stringify(mjBlocks) }}
+        <br>
+        <span>blocks: {{ mjBlocks.filter(b => b.values.length === 3).length }}</span>
+        <br>
+        <span>jantou: {{ mjBlocks.filter(b => b.values.length === 2).length }}</span>
+    </div>
   </div>
 </template>
 
