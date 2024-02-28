0b13d256e7455e5161f9a929bc1ee9b932714f7b
petitbouquet <161125945+petitbouquet@users.noreply.github.com>
2024-02-29 07:54:09 +0900

    impl fu counter

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,43 @@
     import { ref, watch, onBeforeMount } from 'vue'
     import { flattenDeep } from 'lodash-es'
 
+    const KIND = {
+        MANZU: 'manzu',
+        SOUZU: 'souzu',
+        PINZU: 'minzu',
+        YAKUHAI: 'yakuhai'
+    }
+
+    const TYPE = {
+        SYUNTSU: 'syuntsu',
+        KOUTSU: 'koutsu',
+        KANTSU: 'kantsu',
+        TOITSU: 'toitsu',
+        JYANTOU: 'jyantou'
+    }
+
+    const FU_MAPPER = {
+        FUTEI: 20,
+        MENZEN_RON: 10,
+        TSUMO: 2,
+        JYANTOU: 0,
+        JYANTOU_YAKUHAI: 2,
+        YAOTYU_RATE: 2,
+        SYUNTSU: 0,
+        // RON MINKO, TSUMO ANKO
+        MINKO: 2,
+        ANKO: 4,
+        MINKAN: 8,
+        ANKAN: 16,
+        FU_PINFU: 20,
+        FU_CHITOITSU: 25,
+        // APPLY IF FIND SINGLE MACHI PATTERN
+        SINGLE_MACHI: 2
+    }
+
     let mjTehaiStrings = ref('')
     let isValid = ref(false)
+    let fu = ref(0)
 
     let mjActions = ref({
         isReached: false,
@@ -12,15 +47,16 @@
         isIppatu: false,
         isHaitei: false,
         isRinsyan: false,
-        isChankan: false
+        isChankan: false,
+        isMenzen: false
     })
 
     let mjBlocks = ref([
-        { kind: 'manzu', type: 'syuntsu', isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: false, values: [1, 2, 3] },
-        { kind: 'souzu', type: 'syuntsu', isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: false, values: [1, 2, 3] },
-        { kind: 'pinzu', type: 'syuntsu', isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: false, values: [1, 2, 3] },
-        { kind: 'ton', type: 'koutsu', isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: true, values: [1, 1, 1] },
-        { kind: 'haku', type: 'jyanto', isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: true, values: [1, 1] },
+        { kind: KIND.MANZU, type: TYPE.SYUNTSU, isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: false, values: [1, 2, 3] },
+        { kind: KIND.SOUZU, type: TYPE.SYUNTSU, isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: false, values: [1, 2, 3] },
+        { kind: KIND.SOUZU, type: TYPE.SYUNTSU, isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: false, values: [1, 2, 3] },
+        { kind: KIND.YAKUHAI, type: TYPE.KOUTSU, isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: true, values: [1, 1, 1] },
+        { kind:KIND.YAKUHAI, type: TYPE.JYANTOU, isCalled: false, callType: undefined, hasYaoTyu: true, isOnlyYaoTyu: true, values: [1, 1] },
     ])
 
     function tehaiValidator () {
@@ -31,9 +67,40 @@
         isValid = sevenToitsu || (hasOneJantou && hasFourBlock)
     }
 
-    watch(mjBlocks, () => tehaiValidator(), { deep: true })
-    watch(mjActions, () => tehaiValidator(), { deep: true })
-    onBeforeMount(() => tehaiValidator())
+    function countFu () {
+        let fu_temp = FU_MAPPER.FUTEI
+
+        const actions = mjActions.value
+        if (actions.isTsumo) fu_temp += FU_MAPPER.TSUMO
+        if (!actions.isTsumo && actions.isMenzen) fu_temp += FU_MAPPER.MENZEN_RON
+        
+        // TODO PINFU AND CHITOISTU DETECT
+        mjBlocks.value.forEach(b => {
+            const fu_rate = b.hasYaoTyu ? FU_MAPPER.YAOTYU_RATE : 1
+            if (b.type === TYPE.SYUNTSU) fu += FU_MAPPER.SYUNTSU * fu_rate
+            if (b.type === TYPE.KOUTSU) {
+                fu_temp += b.isCalled ? FU_MAPPER.MINKO : FU_MAPPER.ANKO * fu_rate
+            }
+            if (b.type === TYPE.KANTSU) {
+                fu_temp += b.isCalled ? FU_MAPPER.MINKAN : FU_MAPPER.MINKO * fu_rate
+            }
+            if (b.type === TYPE.JYANTOU) {
+                fu_temp += b.kind === KIND.YAKUHAI ? FU_MAPPER.JYANTOU_YAKUHAI : FU_MAPPER.JYANTOU * fu_rate
+            }
+        })
+
+        console.log(fu_temp)
+        fu = Math.ceil(fu_temp / 10) * 10
+    }
+
+    function calcProcess () {
+        tehaiValidator()
+        countFu()     
+    }
+
+    watch(mjBlocks, () => calcProcess, { deep: true })
+    watch(mjActions, () => calcProcess(), { deep: true })
+    onBeforeMount(() => calcProcess())
 </script>
 
 <template>
@@ -41,11 +108,13 @@
     <p>hello</p>
     <input v-model="mjTehaiStrings">
     <p>mjTehaiStrings: {{mjTehaiStrings}} </p>
+    <p>fu: {{ fu }}</p>
     <div>
         {{ JSON.stringify(mjActions) }}
     </div>
     <p>mjActions Button</p>
     <div>
+        <button @click="mjActions.isMenzen = !mjActions.isMenzen">isMenzen</button>
         <button @click="mjActions.isReached = !mjActions.isReached">isReached</button>
         <button @click="mjActions.isDoubleReached = !mjActions.isDoubleReached">isDoubleReached</button>
         <button @click="mjActions.isTsumo = !mjActions.isTsumo">isTsumo</button>
